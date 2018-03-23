aa83c610eb1301566fc3b01bd2684a284e08375f
k3rn3l16 <k3rn3l16@outlook.com>
2018-03-22 19:55:32 -0700

    Refactor categorical list view into separate activity w/ fragment

---
+++ app/src/main/java/com/example/android/tourfc/AttractionListActivity.java
@@ -0,0 +1,33 @@
+package com.example.android.tourfc;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class AttractionListActivity extends AppCompatActivity {
+    private static final String EXTRA_ATTRACTION_TYPE = "om.example.android.tourfc.attraction_type";
+
+    public static Intent newIntent(Context packageContext, String attractionType) {
+        Intent intent = new Intent(packageContext, AttractionListActivity.class);
+        intent.putExtra(EXTRA_ATTRACTION_TYPE, attractionType);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        String attractionType = getIntent().getStringExtra(EXTRA_ATTRACTION_TYPE);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = AttractionListFragment.newInstance(attractionType);
+        fm.beginTransaction()
+                .add(R.id.list_fragment_container, fragment)
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/example/android/tourfc/AttractionListFragment.java
@@ -0,0 +1,70 @@
+package com.example.android.tourfc;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.android.tourfc.model.Attraction;
+import com.example.android.tourfc.model.AttractionCollection;
+import com.example.android.tourfc.model.AttractionRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AttractionListFragment extends Fragment {
+
+    private static final String ARG_ATTRACTION_TYPE = "attractionType";
+    private String attractionType;
+    private ListView attractionListView;
+    private CollectionAdapter listViewAdapter;
+
+    public static AttractionListFragment newInstance(String attractionType) {
+        Bundle args = new Bundle();
+        args.putString(ARG_ATTRACTION_TYPE, attractionType);
+
+        AttractionListFragment fragment = new AttractionListFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        attractionType = getArguments().getString(ARG_ATTRACTION_TYPE);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.show_all_view, container, false);
+        List<Attraction> attractions = new ArrayList<>();
+        SparseArray<AttractionCollection> collection = AttractionRepository.getInstance(getActivity())
+                .getCollections();
+        if (attractionType.equals(getString(R.string.top_activities))) {
+            attractions = collection.get(R.string.top_activities).getAttractions();
+            getActivity().setTitle(R.string.top_activities);
+        } else if (attractionType.equals(getString(R.string.top_bars_nightlife))) {
+            attractions = collection.get(R.string.top_bars_nightlife).getAttractions();
+            getActivity().setTitle(R.string.top_bars_nightlife);
+        } else if (attractionType.equals(getString(R.string.top_breweries))) {
+            attractions = collection.get(R.string.top_breweries).getAttractions();
+            getActivity().setTitle(R.string.top_breweries);
+        } else if (attractionType.equals(getString(R.string.top_restaurants))) {
+            attractions = collection.get(R.string.top_restaurants).getAttractions();
+            getActivity().setTitle(R.string.top_restaurants);
+        }
+
+        attractionListView = v.findViewById(R.id.show_all_list_view);
+        listViewAdapter = new CollectionAdapter(getActivity(), attractions);
+        attractionListView.setAdapter(listViewAdapter);
+
+        return v;
+    }
+}
