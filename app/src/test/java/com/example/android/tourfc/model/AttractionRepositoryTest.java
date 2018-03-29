1afdac12db6d438c1d2dd2fd34a6f3b55a0eb8ae
k3rn3l16 <k3rn3l16@outlook.com>
2018-03-28 23:54:41 -0700

    Add more tests to repository unit & add Robolectric dependencies

--- app/src/test/java/com/example/android/tourfc/model/AttractionRepositoryTest.java
+++ app/src/test/java/com/example/android/tourfc/model/AttractionRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.android.tourfc.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
@@ -7,9 +8,31 @@ import static org.junit.Assert.assertThat;
 
 public class AttractionRepositoryTest {
 
+    private AttractionRepository subject;
+
+    @Before
+    public void setup() {
+        // Nothing for now
+    }
+
     @Test
     public void saveAsAttractionCollection_buildActivityCollection() {
         assertThat(AttractionRepository.buildActivityCollection(), instanceOf(AttractionCollection.class));
+    }
+
+    @Test
+    public void saveAsAttractionCollection_buildRestaurantsCollection() {
+        assertThat(AttractionRepository.buildRestaurantsCollection(), instanceOf(AttractionCollection.class));
+    }
+
+    @Test
+    public void saveAsAttractionCollection_buildBreweriesCollection() {
+        assertThat(AttractionRepository.buildBreweriesCollection(), instanceOf(AttractionCollection.class));
+    }
+
+    @Test
+    public void saveAsAttractionCollection_buildNightLifeCollection() {
+        assertThat(AttractionRepository.buildNightLifeCollection(), instanceOf(AttractionCollection.class));
     }
 
     // TODO - Try injection to write tests for this class
