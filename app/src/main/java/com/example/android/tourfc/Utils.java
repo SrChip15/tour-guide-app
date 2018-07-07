0ab60dacbfab85f7959e71cb0c63e14d7acfe704
Sri <k3rn3l16@outlook.com>
2018-07-06 17:43:12 -0700

    Fix lint warning in Utils

--- app/src/main/java/com/example/android/tourfc/Utils.java
+++ app/src/main/java/com/example/android/tourfc/Utils.java
@@ -5,10 +5,10 @@ import android.content.Context;
 public final class Utils {
 
     public static int resolveStringToResId(Context context, CharSequence text) {
-        if (context.getString(R.string.top_activities).equals(text)) return R.string.top_activities;
-        else if (context.getString(R.string.top_bars_nightlife).equals(text)) return R.string.top_bars_nightlife;
-        else if (context.getString(R.string.top_breweries).equals(text)) return R.string.top_breweries;
-        else if (context.getString(R.string.top_restaurants).equals(text)) return R.string.top_restaurants;
+        if (context.getString(R.string.top_activities).contentEquals(text)) return R.string.top_activities;
+        else if (context.getString(R.string.top_bars_nightlife).contentEquals(text)) return R.string.top_bars_nightlife;
+        else if (context.getString(R.string.top_breweries).contentEquals(text)) return R.string.top_breweries;
+        else if (context.getString(R.string.top_restaurants).contentEquals(text)) return R.string.top_restaurants;
         else return 0;
     }
 }
