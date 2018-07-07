1b6170efb1be774c4c71dbcf18ff01f6d91e6170
Sri <k3rn3l16@outlook.com>
2018-07-06 17:51:07 -0700

    Move image dims compute to ScaledImages & refactor class

--- app/src/main/java/com/example/android/tourfc/ScaledImages.java
+++ app/src/main/java/com/example/android/tourfc/ScaledImages.java
@@ -3,6 +3,7 @@ package com.example.android.tourfc;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.TypedValue;
 
 /**
  * Helper class for scaling down images to efficiently load images
@@ -11,62 +12,64 @@ import android.graphics.BitmapFactory;
  * so that I understand the inner workings of such frameworks as it is my first time
  */
 final class ScaledImages {
-
-	/**
-	 * This method reduces the size of the image to a power of two larger than my target size
-	 *
-	 * @param options   a nested class object of the factory that holds default values for a bunch
-	 *                  of bitmap configurations
-	 * @param reqWidth  an {@link int} value for the target image width
-	 * @param reqHeight an {@link int} value for the target image height
-	 * @return the scaling factor for the image file given its actual bounds
-	 */
-	private static int calculateInSampleSize(
-			BitmapFactory.Options options, int reqWidth, int reqHeight) {
-		// Raw height and width of image
-		final int height = options.outHeight;
-		final int width = options.outWidth;
-		int inSampleSize = 1;
-
-		if (height > reqHeight || width > reqWidth) {
-
-			final int halfHeight = height / 2;
-			final int halfWidth = width / 2;
-
-			// Calculate the largest inSampleSize value that is a power of 2 and keeps both
-			// height and width larger than the requested height and width.
-			while ((halfHeight / inSampleSize) >= reqHeight
-					&& (halfWidth / inSampleSize) >= reqWidth) {
-				inSampleSize *= 2;
-			}
-		}
-
-		return inSampleSize;
-	}
-
-	/**
-	 * This method gets the scaling factor from the calculateInSampleSize method and scales the
-	 * image properly to present an overall nicer looking image
-	 *
-	 * @param res       a Resources object passing the control of the app's resources to access the image
-	 * @param resId     an {@link int} value of the image resource ID
-	 * @param reqWidth  an {@link int} value of the target width of the image
-	 * @param reqHeight an {@link int} value of the target height of the imaage
-	 * @return a scaled down {@link Bitmap} of the image file
-	 */
-	static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-	                                              int reqWidth, int reqHeight) {
-
-		// First decode with inJustDecodeBounds=true to check dimensions
-		final BitmapFactory.Options options = new BitmapFactory.Options();
-		options.inJustDecodeBounds = true;
-		BitmapFactory.decodeResource(res, resId, options);
-
-		// Calculate inSampleSize
-		options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-		// Decode bitmap with inSampleSize set
-		options.inJustDecodeBounds = false;
-		return BitmapFactory.decodeResource(res, resId, options);
-	}
+    
+    /**
+     * This method reduces the size of the image to a power of two larger than my target size
+     *
+     * @param options   a nested class object of the factory that holds default values for a bunch
+     *                  of bitmap configurations
+     * @param reqWidth  an int value for the target image width
+     * @param reqHeight an int value for the target image height
+     * @return the scaling factor for the image file given its actual bounds
+     */
+    private static int calculateInSampleSize(BitmapFactory.Options options,
+                                             int reqWidth, int reqHeight) {
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+        int inSampleSize = 1;
+        
+        if (srcHeight > reqHeight || srcWidth > reqWidth) {
+    
+            float widthScale = srcWidth / 2;
+            float heightScale = srcHeight / 2;
+            
+            inSampleSize = Math.round(heightScale > widthScale ? heightScale : widthScale);
+        }
+        
+        return inSampleSize;
+    }
+    
+    /**
+     * This method gets the scaling factor from the calculateInSampleSize method and scales the
+     * image properly to present an overall nicer looking image
+     *
+     * @param res   a Resources object passing the control of the app's resources to access the image
+     * @param resId an {@link int} value of the image resource ID
+     * @return a scaled down {@link Bitmap} of the image file
+     */
+    static Bitmap decodeSampledBitmapFromResource(Resources res, int resId) {
+        
+        // Convert image's width dimension in dips to pixels
+        int imageWidth = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                300,
+                res.getDisplayMetrics());
+    
+        // Convert image's height dimension in dips to pixels
+        int imageHeight = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                150,
+                res.getDisplayMetrics());
+        
+        // First decode with inJustDecodeBounds=true to check dimensions
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        int inSampleSize = calculateInSampleSize(options, imageWidth, imageHeight);
+        
+        // Decode bitmap with inSampleSize set
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+        
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
 }
